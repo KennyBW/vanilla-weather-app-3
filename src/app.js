aba61b295ec0c2b4a9dfa5cbd5879e4353afe487
Bamidele Kehinde BW <bamidelekehinde.bw2020@gmail.com>
2023-11-10 13:57:02 +0100

    Added Api functionality

--- src/app.js
+++ src/app.js
@@ -1,2 +1,19 @@
+function searchCity(city) {
+    //make API call and update the interface
+    let ApiKey = "9fftde7cf3f78f05f5b8obbaac0cbd44"
+   let apiUrl =
+    `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units="metric"`;
+     console.log(apiUrl);
+
+}
+
+function handleSearchSubmit(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input");
+let cityElement = document.querySelector("#city");
+cityElement.innerHTML = searchInput.value;
+searchCity(searchInput.value);
+}
+
 let searchFormElement = document.querySelector("#search-form");
-console.log(searchFormElement);
+searchFormElement.addEventListener("submit", handleSearchSubmit);
