75faf5b9d479eb173f55a626879ab4d48878acf2
Bamidele Kehinde BW <bamidelekehinde.bw2020@gmail.com>
2023-11-10 17:25:17 +0100

    Update API  JS

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ function searchCity(city) {
     //make API call and update the interface
     let ApiKey = "9fftde7cf3f78f05f5b8obbaac0cbd44"
    let apiUrl =
-    `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units="metric"`;
+    `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
      console.log(apiUrl);
 
 }
