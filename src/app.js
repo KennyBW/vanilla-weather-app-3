a89fb21c110558908794b1c87303ba270e2e2f86
Bamidele Kehinde BW <bamidelekehinde.bw2020@gmail.com>
2023-11-14 12:33:08 +0100

    Added the search functionality using the API

--- src/app.js
+++ src/app.js
@@ -1,19 +1,32 @@
+function refreshWeather(response){
+    let temperatureElement=document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+    
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+
 function searchCity(city) {
     //make API call and update the interface
-    let ApiKey = "9fftde7cf3f78f05f5b8obbaac0cbd44"
-   let apiUrl =
-    `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-     console.log(apiUrl);
+    let apiKey = "8ba535df7ba0c99fc9b12dd999becbec";
 
+   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
     event.preventDefault();
+ 
     let searchInput = document.querySelector("#search-form-input");
+
+    searchCity(searchInput.value);
+
+}
+   /* let searchInput = document.querySelector("#search-form-input");
 let cityElement = document.querySelector("#city");
 cityElement.innerHTML = searchInput.value;
-searchCity(searchInput.value);
-}
+searchCity(searchInput.value);*/
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
